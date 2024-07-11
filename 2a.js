ee2a9c78e3d152bf61e2a8ff4099aee7904012f3
Afy2728 <163610077+Afy2728@users.noreply.github.com>
2024-07-11 21:51:01 +0530

    Update 2a.js

--- 2a.js
+++ 2a.js
@@ -1,14 +1,27 @@
-function fs(){
+[9:44 pm, 11/7/2024] Ajay Cse: function myfunc()
+{
+    var num=parseFloat(prompt("Enter a number: ",0));
+    var num=Math.trunc(num);
 
-    monthNames = ["jan", "feb", "march", "april", "may", "june"]
-    let result = ""
-    var num = document.getElementById("num").value;
-    if(isNaN(num)	||	num<1||	num>6){
-         result = "Bad number"
+    if(num>=1 && num<=12)
+    {
+        function inFunc()
+        {
+            var months=['Jan','Feb','Mar','April','May','June','July','Aug','sept','oct','nov','dec'];
+            return months[num-1];
+
+        }
     }
-    else{
-        num = Math.floor(num)
-      result	= monthNames[num-1];
+    else
+    {
+        return "Bad Number";
     }
-    document.getElementById("result").innerHTML=result;
-    }
+
+    return inFunc;
+
+}
+
+c=myfunc();
+var ans=c();
+
+document.getElementById("res").innerHTML=ans;
